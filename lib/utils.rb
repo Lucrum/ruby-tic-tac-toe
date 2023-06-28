7ea5805b4b63d68cd06d845966c330fec861565a
Elton <28992495+Lucrum@users.noreply.github.com>
2023-06-28 17:23:19 -0400

    Simplified init Prevent players from using the same name or symbol

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,13 +1,43 @@
 # frozen_string_literal: true
 
-def get_symbol(player_name)
-  symbol = ''
+# returns an array of both players
+def init_players
+  player_one_name = get_name(1)
+  player_one_symbol = get_symbol(player_one_name)
+
+  player_two_name = get_name(2, player_one_name)
+  player_two_symbol = get_symbol(player_two_name, player_one_symbol)
+
+  [Player.new(player_one_name, player_one_symbol),
+   Player.new(player_two_name, player_two_symbol)]
+end
+
+def get_symbol(player_name, other_symbol = nil)
   puts "#{player_name}, what symbol would you like on the board?"
   loop do
     symbol = gets.chomp
 
-    return symbol if symbol.length == 1
+    return symbol if symbol.length == 1 && symbol != other_symbol
 
-    puts 'That is not a valid symbol. Please try again.'
+    if symbol == other_symbol
+      puts "The other player has taken #{other_symbol}. Please try again."
+    else
+      puts 'That is not a valid symbol. Please try again.'
+    end
+  end
+end
+
+def get_name(number, other_name = nil)
+  puts "Player #{number}, what is your name?"
+  loop do
+    name = gets.chomp
+
+    return name if !name.empty? && name != other_name
+
+    if name == other_name
+      puts "The other player has taken #{other_name}. Please try again."
+    else
+      puts 'That is not a valid name. Please try again.'
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -5,22 +5,11 @@ require_relative 'lib/utils'
 require_relative 'lib/tictactoe'
 
 # initialize players
-puts 'Player 1, what is your name?'
+players = init_players
 
-player_one_name = gets.chomp
-player_one_symbol = get_symbol(player_one_name)
+puts "#{players[0].name} and #{players[1].name}, welcome"
 
-puts 'Player 2, what is your name?'
-
-player_two_name = gets.chomp
-player_two_symbol = get_symbol(player_two_name)
-
-player_one = Player.new(player_one_name, player_one_symbol)
-player_two = Player.new(player_two_name, player_two_symbol)
-
-puts "#{player_one.name} and #{player_two.name}, welcome"
-
-game = TicTacToe.new(player_one, player_two)
+game = TicTacToe.new(players[0], players[1])
 game.play
 
 # initialize game
